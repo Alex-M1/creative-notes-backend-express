ef77ba0244120bd96e4c2fdefffe32b2a07b4913
Alex Malyk <oleksiimal@wizardsdev.com>
2021-09-26 22:40:11 +0300

    add readme

--- src/constants/messages.ts
+++ src/constants/messages.ts
@@ -3,5 +3,6 @@ export const MESSAGES = {
   success: 'success',
   user_not_found: 'user_not_found',
   invalid_password: 'invalid_password',
+  invalid_login: 'invalid_login',
   abstract_err: 'something_wrong',
 };
